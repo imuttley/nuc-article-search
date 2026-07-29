57f872fb7c5ccfed703cfbf1a7646835ac14e763
tommaso nicosia <tommaso.nicosia@enea.it>
2026-07-29 07:07:04 +0000

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -3,23 +3,10 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p dist
 
-node -e "
-var spawn = require('child_process').spawnSync;
-var fs = require('fs');
-var r = spawn('/home/nicosia/.bun/bin/bun', [
-  'build', './src/index.tsx',
-  '--outdir', './dist',
-  '--target', 'browser',
-  '--format', 'iife',
-  '--minify'
-], {encoding:'utf8', cwd: '/home/nicosia/articoli-nucleare/joomla-component'});
-process.stdout.write(r.stdout);
-process.stderr.write(r.stderr);
-if (r.status !== 0) process.exit(r.status);
-var idx = '/home/nicosia/articoli-nucleare/joomla-component/dist/index.js';
-var dst = '/home/nicosia/articoli-nucleare/joomla-component/dist/document-search.js';
-if (fs.existsSync(idx)) {
-  fs.renameSync(idx, dst);
-  console.log('Renamed to document-search.js');
-}
-"
+bun build ./src/index.tsx --outdir ./dist --target browser --format iife --minify
+
+if [ -f ./dist/index.js ]; then
+  mv ./dist/index.js ./dist/document-search.js
+  echo 'Renamed to document-search.js'
+fi
+
